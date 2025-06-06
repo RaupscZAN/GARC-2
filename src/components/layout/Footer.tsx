6b4e1fc9eeb3d33ae0f66b9602ab641b7d774c94
RaupscZAN <ra.upsc@edgeup.in>
2025-06-06 22:16:01 +0530

    Updated Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -13,8 +13,8 @@ const Footer: React.FC = () => {
               <img 
                 src="https://tfvzrxzmsltgwtkotmyg.supabase.co/storage/v1/object/sign/garc-images/logo.png?token=eyJraWQiOiJzdG9yYWdlLXVybC1zaWduaW5nLWtleV83ZDljN2MwZS05ZWEyLTQxOGUtOTE2Zi05NjY2NDk5YTA4MjYiLCJhbGciOiJIUzI1NiJ9.eyJ1cmwiOiJnYXJjLWltYWdlcy9sb2dvLnBuZyIsImlhdCI6MTc0OTIxNzQ3MCwiZXhwIjoyNjEzMjE3NDcwfQ.XJjG0Jm8La7ShNmXLN2DK6fDl_7Ac6rnKcWKv_ue6fM" 
                 alt="GARC Logo"
-                width="200"
-                height="200"
+                width="300"
+                height="300"
                 className="object-contain"
               />
             </Link>
